9420d5a7a6f0bc3ca1313264e3e7fda4ef0cba86
elrenv <renanzitovgl@gmail.com>
2021-10-04 09:10:46 -0300

    adicinado evento de drop e função de tempo total de jogo

--- rnv_playtime/rnv_server.lua
+++ rnv_playtime/rnv_server.lua
@@ -24,10 +24,30 @@ function rnv.cachehoralogin(user_id)
     return cache[user_id]
 end
 
+function rnv.tempototal(user_id)
+	tempoentrada = rnv.cachehoralogin(user_id)
+	end_time = os.time()
+	tempodecorrido = os.difftime(end_time, tempoentrada)
+    local hours = math.floor(tempodecorrido/3600)
+    tempodecorrido = tempodecorrido - hours * 3600
+    local minutes = math.floor(tempodecorrido/60)
+    tempodecorrido = tempodecorrido - minutes * 60
+	valorhoras = string.format("%d Horas, %d Minutos e %d Segundos",hours,minutes,tempodecorrido)
+	SendWebhookMessage(webhookhoras,"```prolog\n[ID]: "..user_id.." \n[=========SAIU DO SERVIDOR=========]\nTEMPO DE JOGO: "..valorhoras.."\n"..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
+end
+
 AddEventHandler("vRP:playerSpawn",function(user_id,source,first_spawn)
 	local source = source
     local user_id = vRP.getUserId(source)
 	if first_spawn then
 		rnv.horalogin(user_id)
 	end
+end)
+
+AddEventHandler("playerDropped", function(user_id)
+    local source = source
+    local user_id = vRP.getUserId(source)
+    if user_id then
+		rnv.tempototal(user_id)
+    end
 end)
